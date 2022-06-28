2b096448b11f5925d26d08b5218422f35444e42a
cochimu <cochimu1221@icloud.com>
2022-06-28 23:47:37 +0900

    5

--- src/App.vue
+++ src/App.vue
@@ -8,14 +8,22 @@ const addTodo = () => {
   todos.value.push(newTodo.value)
   newTodo.value = ''
 }
+
+const removeTodo = (index) => {
+  todos.value.splice(index, 1)
+}
 </script>
 
 <template>
 <input type="text" size="30" v-model="newTodo">
 <button @click="addTodo()">+1追加する</button>
 
-<ul>
-  <li v-for=" (todo, i) in todos" :key="i">{{ todo }}</li>
+<!-- v-ifで要素がある時だけulタグを出す -->
+<!-- v-showでdisplay:noneを付与する方法もある -->
+<ul v-if="todos.length > 0">
+  <li v-for=" (todo, i) in todos" :key="i">{{ todo }} <span @click="removeTodo(i)" style="cursor: pointer">x</span></li>
 </ul>
+<!-- v-else -->
+ <p v-else>※ ToDoを追加してください</p>
 
 </template>
